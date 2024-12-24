f262ac66061f3904bb690a0f5cf65b504ec03a4c
4real4real247 <cassy.h702@gmail.com>
2024-12-23 21:10:53 -0800

    main

---
+++ script.js
@@ -0,0 +1,29 @@
+/* INSTRUCTIONS 
+Create a layout for this holiday card! Your card should look like this: 
+https://assets.codepen.io/t-15440/itp-holiday-card.jpg
+
+Responsive design is NOT required. Remember to review old CodePens if you've forgotten how to do anything!
+
+1. Find the font "Mountains of Christmas" on Google Fonts. Assign this font to display on your site.
+
+2. Set up a background on the body. 
+Color: #f0f4f9
+Image: https://assets.codepen.io/t-15440/itp-holiday-bkgd.jpg
+The image should not repeat
+Have the image centered on the page, and aligned to the top of the window.
+
+HINT: Forgotten how to do this? See https://developer.mozilla.org/en-US/docs/Web/CSS/background
+
+BONUS: Investigate the background-size property and see if this will help manage the size of the image...
+
+3. Style the H1 so that it displays the Mountains of Christmas font, font color #C60F0F, size of 3rem, text aligned center. Give it some margin or padding so that it's not on top of the pinecones or the images below.
+
+4. Set your images to display with 4 rounded corners to make a circle. (Stuck? Google for "css circle")
+
+Give each image a red (#C60F0F) border.
+
+5. Use CSS Grid to set all three circles next to each other. Give them a little space in between.
+
+6. You're done for today! Go celebrate!
+
+*/
